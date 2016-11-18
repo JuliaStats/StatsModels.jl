64f021d232b663266c2a4f262e72a36d3408f19e
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2016-11-17 20:17:52 -0500

    include material in deps

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,6 @@ using Documenter, StatsModels
 makedocs()
 
 deploydocs(
-    deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
+    deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-material"),
     repo = "github.com/JuliaStats/DataFrames.jl.git"
 )
