e89de836fed4042c571c71218f5f237f5aaed288
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2016-11-17 22:00:43 -0500

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -4,5 +4,5 @@ makedocs()
 
 deploydocs(
     deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-material"),
-    repo = "github.com/JuliaStats/DataFrames.jl.git"
+    repo = "github.com/JuliaStats/StatsModels.jl.git"
 )
