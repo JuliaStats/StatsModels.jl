5dbab55013765805f5763dad1e75dd603d7a5dd8
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2016-11-18 14:22:26 -0500

    use native HTML output

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,18 @@
 using Documenter, StatsModels
 
-makedocs()
+makedocs(
+    format = :html,
+    sitename = "StatsModels.jl",
+    pages = [
+        "Introduction" => "index.md",
+        "Modeling tabular data" => "formula.md",
+        "Contrast coding categorical variables" => "contrasts.md"
+    ]
+)
 
 deploydocs(
-    deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-material"),
-    repo = "github.com/JuliaStats/StatsModels.jl.git"
+    repo = "github.com/JuliaStats/StatsModels.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing
 )
