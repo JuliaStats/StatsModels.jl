fa32751d3617e6d7e72c5f0c3648ac37df363614
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-04-04 17:27:06 -0400

    update model matrix with working tuples iterator

--- src/modelmatrix2.jl
+++ src/modelmatrix2.jl
@@ -11,7 +11,7 @@
 # * fill in each term's columns in place
 
 
-using StatsModels, DataFrames
+using StatsModels, DataTables
 import StatsModels: AbstractTerm, Term, EvalTerm, ContrastsMatrix, FullDummyCoding
 
 # TODO: seems like we'd actually want to NOT store source on each term, for
@@ -38,9 +38,9 @@ Base.string{C}(t::CategoricalTerm{C}) = "$(t.name)($C)"
 is_categorical(::Union{CategoricalArray, NullableCategoricalArray}) = true
 is_categorical(::Any) = false
 
-is_categorical(name::Symbol, source::AbstractDataFrame) = is_categorical(source[name])
+is_categorical(name::Symbol, source::AbstractDataTable) = is_categorical(source[name])
 
-function set_schema!(terms::Term{:+}, source::AbstractDataFrame)
+function set_schema!(terms::Term{:+}, source::AbstractDataTable)
     already = Set()
     map!(t -> set_schema!(t, already, source), terms.children)
     terms
@@ -83,6 +83,8 @@ end
 
 # set_schema!(x::Any, already::Set, source) = (push!(already, Set([x])); x)
 
+set_schema!(t::Term{1}, already::Set, source) = (push!(already, push!(Set(), t)); t)
+
 # what to do about set_schema! when schema's already been set? could just error,
 # or better yet check whether schema matches.  for categorical terms, can
 # instantiate the contrasts matrix again adn that will do the check?
@@ -100,7 +102,7 @@ function set_schema!(term::CategoricalTerm, aliases::Set, already::Set, source)
     if is_categorical(term.name, source)
         return CategoricalTerm(term.name,
                                ContrastsMatrix(term.contrasts,
-                                               source[term.name]),
+                                               levels(source[term.name])),
                                source)
     else
         throw(ArgumentError("Term $(term) is categorical but $(term.name) is" *
@@ -140,14 +142,14 @@ function modelmat_cols!(dest::AbstractArray, t::CategoricalTerm)
     copy!(dest, t.contrasts.matrix[reindex[v.refs], :])
 end
 
-function model_matrix(terms::AbstractTerm, data::AbstractDataFrame)
+function model_matrix(terms::AbstractTerm, data::AbstractDataTable)
     
     terms = set_schema!(Term{:+}(terms), data)
 
     term_sizes = map(nc, terms.children)
     mat_size = (size(data, 1), sum(term_sizes))
 
-    mat = Matrix(mat_size...)
+    mat = Matrix{Float64}(mat_size...)
 
     first_col = 0
     for t in terms.children
@@ -181,7 +183,7 @@ end
 
 import StatsModels: term
 
-d = DataFrame(a = 1:10, b = categorical(repeat(["a", "b"], outer=5)))
+d = DataTable(a = 1:10, b = categorical(repeat(["a", "b"], outer=5)))
 
 contrasts(t::Term) = map(contrasts, t.children)
 contrasts(t::Term{1}) = nothing
@@ -193,4 +195,88 @@ t2 = set_schema!(term(:(1+a+b)), d)
 
 t3 = set_schema!(term(:(a+b+a&b)), d)
 t4 = set_schema!(term(:(1+a+b+a&b)), d)
+
+model_matrix(t4, d)
+
+
+################################################################################
+# Another strategy: generate an anonymous function that takes tuples and fills
+# in one row of model matrix.
+#
+# Can do this directly from a schema and an expression, or a Term.
+#
+# Term{1} -> 1.
+# ContinuousTerm -> look up colnum from name, index into
+# CategoricalTerm -> look up level from colnum from name, index into contrasts mat
+# Term{:&} -> generate expressions for each child, combine with :kron(...)
+# Term{:+} -> generate expressions for each child, get number of elems for each
+#             child, generate indexing exprs.
+
+
+# generate a _mutating_ anonymous function that takes a (view of) a model matrix
+# row and a tuple of table fields, and fills the model matrix row.
+function anon_factory(terms::Term{:+}, d::AbstractDataTable)
+    col_nums = Dict(k=>i for (i,k) in enumerate(names(d)))
+    out_sym = gensym("Modelmat row")
+    tuple_sym = gensym("Data tuple")
+    # a begin ... end block for the body of the anon func
+    term_exs = Expr(:block)
+    # current starting index
+    i = 1
+    for term in terms.children
+        term_ex, n_cols = term_ex_factory(term, tuple_sym, col_nums)
+        if n_cols > 0
+            push!(term_exs.args, :($out_sym[$i:$(i+n_cols-1)] = $term_ex))
+        end
+        i += n_cols
+    end
+    :(($out_sym, $tuple_sym) -> $term_exs)
+end
+
+
+# extract the names of all the columns that will be references in the data
+dat_cols(t::ContinuousTerm) = t.name
+dat_cols(t::CategoricalTerm) = t.name
+dat_cols(t::Term{:&}) = mapreduce(dat_cols, vcat, t.children)
+dat_cols(t::Term{:+}) = unique(mapreduce(dat_cols, vcat, t.children))
+dat_cols(t::Any) = Symbol[]
+
+
+# generate expressions for a single term that will be assigned to the mm row
+term_ex_factory(::Term{1}, tup, cols) = 1, 1
+term_ex_factory(::Term{0}, tup, cols) = :(), 0
+term_ex_factory(t::ContinuousTerm, tup, cols) = :(get($tup[$(cols[t.name])])), 1
+function term_ex_factory(t::CategoricalTerm, tup, cols)
+    # splice in the contrast matrix literally
+    # TODO: make sure levels in the data line up here
+    contr_mat = t.contrasts.matrix
+    # number of columns in contrasts/model matrix
+    n_cols = size(contr_mat, 2)
+    :($contr_mat[get($tup[$(cols[t.name])]).level, :]), n_cols
+end
+
+function term_ex_factory(t::Term{:&}, tup, cols)
+    children, n_cols = zip([term_ex_factory(c, tup, cols) for c in t.children]...)
+    :(kron($(children...))), prod(n_cols)
+end
+
+
+
+function model_matrix_tuples(terms::AbstractTerm, data::AbstractDataTable)
+    terms = set_schema!(Term{:+}(terms), data)
+    term_sizes = map(nc, terms.children)
+    mat_size = (size(data, 1), sum(term_sizes))
+    mat = Matrix{Float64}(mat_size...)
+    fill_row! = eval(anon_factory(terms, data))
+    tuples = zip((data[name] for name in names(data))...)
+    for (i,t) in enumerate(tuples)
+        fill_row!(view(mat, i, :), t)
+    end
+    mat
+end
+
+model_matrix_tuples(t1, d)
+model_matrix_tuples(t2, d)
+model_matrix_tuples(t3, d)
+model_matrix_tuples(t4, d)
 
