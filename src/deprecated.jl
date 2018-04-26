c320c42daed1382838a2c70d62cf729fe6fb5d6f
Matthieu Gomez <gomez.matthieu@gmail.com>
2018-04-26 16:49:53 -0400

    Escape $ to avoid interpolations

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,5 +1,5 @@
 function Formula(lhs, rhs)
-    Base.depwarn("Formula(lhs, rhs) is deprecated. Use @eval(@formula($lhs ~ $rhs)) if " *
+    Base.depwarn("Formula(lhs, rhs) is deprecated. Use @eval(@formula(\$lhs ~ \$rhs)) if " *
                  "parsing is required, or Formula(ex_orig, ex, lhs, rhs) if not",
                  :Formula)
     Formula(:(), :($lhs ~ $rhs), lhs, rhs)
