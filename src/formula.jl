358ac413b1c86a85f12aaa837d557d7bc4443037
Asaf Manela <AsafManela@users.noreply.github.com>
2018-06-27 10:44:21 -0500

    fixes copy(::Formula) when rhs is a single Symbol

--- src/formula.jl
+++ src/formula.jl
@@ -360,10 +360,11 @@ function Formula(t::Terms)
     Formula(ex, ex, lhs,rhs)
 end
 
+copyside(s) = copy(s)
+copyside(s::Symbol) = s
+
 function Base.copy(f::Formula)
-    lhs = isa(f.lhs, Symbol) ? f.lhs : copy(f.lhs)
-    rhs = isa(f.rhs, Symbol) ? f.rhs : copy(f.rhs)
-    return Formula(copy(f.ex_orig), copy(f.ex), lhs, rhs)
+    return Formula(copy(f.ex_orig), copy(f.ex), copyside(f.lhs), copyside(f.rhs))
 end
 
 """
