610cc027673c962c6dcf33d55af0acb08fee592c
Asaf Manela <AsafManela@users.noreply.github.com>
2018-09-29 17:03:06 -0500

    reverted end of line changes

--- src/statsmodel.jl
+++ src/statsmodel.jl
@@ -48,12 +48,12 @@ end
 """
     drop_intercept(::Type)
 
-Define whether a given model automatically drops the intercept. Return `false` by default.
-To specify that a model type `T` drops the intercept, overload this function for the
+Define whether a given model automatically drops the intercept. Return `false` by default. 
+To specify that a model type `T` drops the intercept, overload this function for the 
 corresponding type: `drop_intercept(::Type{T}) = true`
 
-Models that drop the intercept will be fitted without one: the intercept term will be
-removed even if explicitly provided by the user. Categorical variables will be expanded
+Models that drop the intercept will be fitted without one: the intercept term will be 
+removed even if explicitly provided by the user. Categorical variables will be expanded 
 in the rank-reduced form (contrasts for `n` levels will only produce `n-1` columns).
 """
 drop_intercept(::Type) = false
