25f8e3c13057d78f82f819be23724eac8bcdba08
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2019-02-07 15:09:28 -0500

    test show methods with mime

--- test/terms.jl
+++ test/terms.jl
@@ -1,3 +1,10 @@
+function mimestring(mime::Type{<:MIME}, x)
+    buf=IOBuffer()
+    show(buf, mime(), x)
+    String(take!(buf))
+end
+mimestring(x) = mimestring(MIME"text/plain", x)
+
 @testset "terms" begin
 
     using Statistics
@@ -5,9 +12,11 @@
     @testset "concrete instantion with schema" begin
         t = term(:aaa)
         @test string(t) == "aaa"
+        @test mimestring(t) == "aaa(unknown)"
 
         t0 = schema(t, [3, 2, 1])
-        @test string(t0) == "aaa (continuous)"
+        @test string(t0) == "aaa"
+        @test mimestring(t0) == "aaa(continuous)"
         @test t0.mean == 2.0
         @test t0.var == var([1,2,3])
         @test t0.min == 1.0
@@ -15,19 +24,23 @@
 
         t1 = schema(t, [:a, :b, :c])
         @test t1.contrasts isa StatsModels.ContrastsMatrix{DummyCoding}
-        @test string(t1) == "aaa (3 levels): DummyCoding(2)"
+        @test string(t1) == "aaa"
+        @test mimestring(t1) == "aaa(DummyCoding:3→2)"
 
         t3 = schema(t, [:a, :b, :c], DummyCoding())
         @test t3.contrasts isa StatsModels.ContrastsMatrix{DummyCoding}
-        @test string(t3) == "aaa (3 levels): DummyCoding(2)"
+        @test string(t3) == "aaa"
+        @test mimestring(t3) == "aaa(DummyCoding:3→2)"
 
         t2 = schema(t, [:a, :a, :b], EffectsCoding())
         @test t2.contrasts isa StatsModels.ContrastsMatrix{EffectsCoding}
-        @test string(t2) == "aaa (2 levels): EffectsCoding(1)"
+        @test mimestring(t2) == "aaa(EffectsCoding:2→1)"
+        @test string(t2) == "aaa"
 
         t2full = schema(t, [:a, :a, :b], StatsModels.FullDummyCoding())
         @test t2full.contrasts isa StatsModels.ContrastsMatrix{StatsModels.FullDummyCoding}
-        @test string(t2full) == "aaa (2 levels): StatsModels.FullDummyCoding(2)"
+        @test mimestring(t2full) == "aaa(StatsModels.FullDummyCoding:2→2)"
+        @test string(t2full) == "aaa"
     end
     
     @testset "term operators" begin
@@ -36,9 +49,17 @@
         @test a + b == (a, b)
         @test (a ~ b) == FormulaTerm(a, b)
         @test string(a~b) == "$a ~ $b"
+        @test mimestring(a~b) ==
+            strip("""
+FormulaTerm
+Response:
+  a(unknown)
+Predictors:
+  b(unknown)
+""")
         @test a & b == InteractionTerm((a,b))
         @test string(a & b) == "$a & $b"
-
+        @test mimestring(a & b) == "a(unknown) & b(unknown)"
         c = term(:c)
         @test (a+b)+c == (a,b,c)
         @test a+(b+c) == (a,b,c)
