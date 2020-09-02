de636c47aa5998733fb4043af7ccc78ca5bcb541
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2020-09-01 23:18:10 -0400

    missing method for has_schema

--- src/schema.jl
+++ src/schema.jl
@@ -246,6 +246,7 @@ has_schema(t::Term) = false
 has_schema(t::Union{ContinuousTerm,CategoricalTerm}) = true
 has_schema(t::InteractionTerm) = all(has_schema(tt) for tt in t.terms)
 has_schema(t::TupleTerm) = all(has_schema(tt) for tt in t)
+has_schema(t::MatrixTerm) = has_schema(t.terms)
 has_schema(t::FormulaTerm) = has_schema(t.lhs) && has_schema(t.rhs)
 
 struct FullRank
