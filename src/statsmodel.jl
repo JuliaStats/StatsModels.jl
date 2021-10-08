395749323ff43afe957e770a05dadf9d3b7d8cc2
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2021-10-08 12:27:57 -0400

    add isnested method for TableRegressionModel

--- src/statsmodel.jl
+++ src/statsmodel.jl
@@ -144,6 +144,10 @@ StatsBase.r2(mm::TableRegressionModel, variant::Symbol) = r2(mm.model, variant)
 StatsBase.adjr2(mm::TableRegressionModel, variant::Symbol) = adjr2(mm.model, variant)
 StatsBase.loglikelihood(mm::TableModels, c::Colon) = loglikelihood(mm.model, c)
 
+function isnested(m1::TableRegressionModel, m2::TableRegressionModel; kwargs...)
+    return isnested(m1.model, m2.model; kwargs...)
+end
+
 function _return_predictions(T, yp::AbstractVector, nonmissings, len)
     out = Vector{Union{eltype(yp),Missing}}(missing, len)
     out[nonmissings] = yp
